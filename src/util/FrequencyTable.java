78ec39d868a1d76f1c71c70c8c8d049bde73a903
zhaomanzhou <zmzsstreet@163.com>
2019-01-08 18:48:28 +0800

    Update FrequencyTable.java

--- src/util/FrequencyTable.java
+++ src/util/FrequencyTable.java
@@ -6,7 +6,7 @@ public class FrequencyTable {
         byte value;
         Entry next;
     }
-    //默认桶的代码
+    //默认桶的长度 2^4
     private final int DEFAULT_BUCKET_SIZE = 2 >> 4;
     private Entry[] bucket;
     private int bucketLength;
@@ -33,9 +33,13 @@ public class FrequencyTable {
         bucket = new Entry[bucketLength];
         size = 0;
     }
+    
+    
     public void put(byte b)
     {
+        //对应桶的下标
         int index = hash(b)%bucketLength;
+        //如果桶的这个没元素，直接插
         if(bucket[index] == null)
         {
             Entry e = new Entry();
@@ -46,10 +50,13 @@ public class FrequencyTable {
         {
             Entry e = bucket[index];
             for(;e != null && e.key != b; e = e.next);
+            //如果已经存在这个元素
             if(e != null)
             {
                 e.value++;
-            }else{
+            }else
+            //通里有元素的情况
+            {
                 for(e = bucket[index];e.next != null; e = e.next);
                 Entry e1 = new Entry();
                 e1.key = b;
